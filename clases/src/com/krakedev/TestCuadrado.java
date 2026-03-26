1444e9753263f40078a47d1afbb4447fa5de4228
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-26 01:57:24 -0500

    se suben reto 10

---
+++ clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,45 @@
+package com.krakedev;
+
+public class TestCuadrado {
+	
+	public static void main(String[] args) {
+		
+	Cuadrado r1= new Cuadrado();
+	Cuadrado r2= new Cuadrado();
+	Cuadrado r3= new Cuadrado();
+
+	
+	double areaR1;
+	double areaR2;
+	double areaR3;
+	double perimetroR1;
+	double perimetroR2;
+	double perimetroR3;
+	
+	
+	r1.lado=10;
+	r2.lado=8;
+	r3.lado=5;
+
+	
+	areaR1=r1.calcularArea();
+	areaR2=r2.calcularArea();
+	areaR3=r3.calcularArea();
+	
+	perimetroR1=r1.calcularPerimetro();
+	perimetroR2=r2.calcularPerimetro();
+	perimetroR3=r3.calcularPerimetro();
+	
+	System.out.println("Area de r1: " +areaR1);
+	System.out.println("Area de r2: " +areaR2);
+	System.out.println("Area de r3: " +areaR3);
+	
+	System.out.println("Perimetro de r1: " +perimetroR1);
+	System.out.println("Perimetro de r2: " +perimetroR2);
+	System.out.println("Perimetro de r3: " +perimetroR3);
+	
+
+}
+
+
+}
